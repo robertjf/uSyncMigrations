54370c4813daf38af7e3930e80de23333e7171e4
Robert Foster <robert@youritgroup.com.au>
2024-07-23 13:48:41 +1000

    Added Markdown Property support

---
+++ uSync.Migrations.Migrators/Core/MarkdownMigrator.cs
@@ -0,0 +1,18 @@
+﻿using Umbraco.Cms.Core.PropertyEditors;
+using uSync.Migrations.Core.Extensions;
+
+namespace uSync.Migrations.Migrators.Core;
+
+[SyncMigrator(UmbEditors.Aliases.MarkdownEditor, typeof(MarkdownConfiguration), IsDefaultAlias = true)]
+public class MarkdownMigrator : SyncPropertyMigratorBase
+{
+    public override object? GetConfigValues(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
+    {
+        var config = new MarkdownConfiguration {
+            DefaultValue = dataTypeProperty.PreValues.GetPreValueOrDefault("defaultValue", ValueTypes.String),
+            DisplayLivePreview = dataTypeProperty.PreValues.GetPreValueOrDefault("preview", false),
+            OverlaySize = "small"
+        };
+        return config;
+    }
+}
